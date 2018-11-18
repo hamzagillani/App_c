66883a4763479e5d6b3b6d9ef113007dde76f0eb
hamzagillani <hamza.gillani10@gmail.com>
2018-11-19 02:30:04 +0500

    Initial commit

---
+++ app/src/main/java/com/digicon_valley/app_c/Main_Activity_three.java
@@ -0,0 +1,13 @@
+package com.digicon_valley.app_c;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main_Activity_three extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__three);
+    }
+}
